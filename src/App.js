e1e195e6b41a49aa676f474e25fde1cd56261185
petrh-bizcover <petrh@bizcover.com.au>
2020-08-18 12:44:12 +1000

    router added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Home from './home/Home';
+import Chat from './chat/Chat';
+
 import './App.css';
 
 function App() {
@@ -8,7 +18,28 @@ function App() {
         <h1>Huuudovo.com live!</h1>
       </header>
       <content>
-        under construction
+        <Router>
+          <sidebar>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/chat">Chat</Link>
+              </li>
+            </ul>
+          </sidebar>
+          <article>
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/chat">
+                <Chat />
+              </Route>
+            </Switch>
+          </article>
+        </Router>
       </content>
       <footer>
         <span>&copy; Huuuda</span>
