11f2affae80afe62103cac336abe0a491dddcf5a
petrh-bizcover <petrh@bizcover.com.au>
2020-08-18 12:03:39 +1000

    basic updates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Huuudovo.com live!</h1>
       </header>
+      <content>
+        under construction
+      </content>
+      <footer>
+        {`${process.env.REACT_APP_NAME} ${process.env.REACT_APP_VERSION}`}
+      </footer>
     </div>
   );
 }
